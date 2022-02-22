2db9dd42c3a974990093ae315813b711c62d4a2b
Yajush Vyas <dryashhoney@gmail.com>
2022-02-22 15:33:49 +0530

    added courses

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 
 const Main = () => (
-        <div className=" bg-backg flex flex-col h-full w-full">
-                
-        </div>
+      <div className=" bg-backg flex flex-col h-full w-full ">
+        <div className=' mt-10 ml-[21%] flex gap-7'>
+          <div className='flex flex-col place-items-center w-[30%] h-full bg-white rounded-3xl shadow-md shadow-slate-300'>
+            <img src="https://cdn.discordapp.com/attachments/941091409509896283/945618708330577950/giphy.gif" alt="" className='-mt-5 rounded-3xl w-[92%]'/>
+            <span className=' pt-4 w-[90%] text-sm font-bold'> ADVANCED PROBABILITY AND STATISTICS</span>
+            <span className=' pb-5 pt-1 w-[90%] text-xs text-gray-300 font-bold'> 22E-20CST-283</span>
+          </div>
+          <div className='flex flex-col place-items-center w-[30%] h-full bg-white rounded-3xl shadow-md shadow-slate-300'>
+            <img src="https://cdn.discordapp.com/attachments/941091409509896283/945618708330577950/giphy.gif" alt="" className='-mt-5 rounded-3xl w-[92%]'/>
+            <span className=' pt-4 w-[90%] text-sm font-bold'> ADVANCED PROBABILITY AND STATISTICS</span>
+            <span className=' pb-5 pt-1 w-[90%] text-xs text-gray-300 font-bold'> 22E-20CST-283</span>
+          </div>
+          <div className='flex flex-col place-items-center w-[30%] h-full bg-white rounded-3xl shadow-md shadow-slate-300'>
+            <img src="https://cdn.discordapp.com/attachments/941091409509896283/945618708330577950/giphy.gif" alt="" className='-mt-5 rounded-3xl w-[92%]'/>
+            <span className=' pt-4 w-[90%] text-sm font-bold'> ADVANCED PROBABILITY AND STATISTICS</span>
+            <span className=' pb-5 pt-1 w-[90%] text-xs text-gray-300 font-bold'> 22E-20CST-283</span>
+          </div>      
+        </div>           
+      </div>
   );
   
 export default Main;
